51bcfba72d53ed5a399bf7cafa1e292103533309
wolfishflow <alancarlsimon@gmail.com>
2016-03-05 13:57:03 -0500

    Update 1
    Included a JS script that enables the menu. Currently working on GUI

---
+++ script/basejs.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    $(".button-collapse").sideNav();
+    
+    
+    
+    
+    console.log("uh");
+    
+    
+    function menuOnClick() {
+        $('.button-collapse').sideNav('toggle');
+      }
+});
+
+        
